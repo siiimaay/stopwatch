a29f5bdedc0cf653da8369dba5bcde303001b27f
simay <simay.ekici@grubtech.com>
2023-11-10 22:18:15 +0300

    [+] Authentication view: Add app auth and login pages

--- lib/features/login/presentation/login_view.dart
+++ lib/features/login/presentation/login_view.dart
@@ -1,13 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:stopwatch/features/login/presentation/common_widgets/form_field_label.dart';
+import 'package:stopwatch/routes/route_config.dart';
+import 'package:stopwatch/widgets/action_button.dart';
 
-class LoginView extends StatelessWidget {
+import 'common_widgets/text_field.dart';
+
+class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
   @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Hello!"),
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+            color: Colors.indigo,
+            onPressed: () =>
+                GoRouter.of(context).pushNamed(AppRouter.authRoute)),
+        centerTitle: true,
+        title: const Text(
+          "Login",
+          style: TextStyle(color: Colors.indigo, fontWeight: FontWeight.w500),
+        ),
+        elevation: 0.8,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color(0xFFD1C4E9),
+            Color(0xFFBBDEFB),
+          ])),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 13),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FormFieldLabel(
+              label: 'Email',
+            ),
+            Flexible(
+                child: AuthTextField(
+              controller: _emailController,
+            )),
+            FormFieldLabel(
+              label: 'Password',
+            ),
+            Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: AuthTextField(controller: _passwordController)),
+            Center(
+              child: ActionButton(
+                buttonText: "Login",
+                onPressed: () {},
+                textColor: Colors.indigo,
+                buttonSize: Size(200, 50),
+                gradient: const LinearGradient(colors: [
+                  Color(0xFFD1C4E9),
+                  Color(0xFFBBDEFB),
+                ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
