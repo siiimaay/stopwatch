83a2c86abeb43bd0e30e88880b56e2c252467786
simay <simay.ekici@grubtech.com>
2023-11-12 14:43:15 +0300

    [+] configure firebase/firestore for crud and auth operations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stopwatch/features/app/app_view.dart';
 
-void main() {
+import 'core/injection/locator.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
+  await Firebase.initializeApp();
+
   runApp(StopWatchAppView());
 }
