a7d8ed1533d12cf0eccfb8ab5af83d8b561968d5
simay <simay.ekici@grubtech.com>
2023-11-12 20:54:36 +0300

    [+] add services to di

--- lib/core/injection/locator.dart
+++ lib/core/injection/locator.dart
@@ -1,8 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:stopwatch/features/auth/domain/login_service.dart';
 
+import '../../features/stopwatch/data/repository/stopwatch_repository.dart';
+import '../../features/stopwatch/domain/stopwatch_data_service.dart';
+
 final getIt = GetIt.I;
 
 void setupLocator() {
   getIt.registerLazySingleton(() => LoginService());
+  getIt.registerLazySingleton(() => StopWatchStorageService());
+  getIt.registerFactory(() => StopwatchRepository());
 }
